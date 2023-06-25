c4adad45e336724f55e32db274121fa8762ec6db
Runsoo <imapples@naver.com>
2023-06-25 16:23:10 +0900

    10 Input State 바인딩

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,26 +1,22 @@
-// import { Fragment } from "react";
 import { useState } from "react";
 
 function App() {
-  console.log("render");
-  const [number, setNumber] = useState(1);
-  const double = () => {
-    // const doubledNumber = number * 2;
-    setNumber((prevState) => prevState * 2); // 한 줄로 쓸 수 있음
-    setNumber((prevState) => {
-      return prevState * 2;
-    });
-    console.log();
-  };
+  const [title, setTitle] = useState("");
 
   return (
-    <>
-      <div>{number}</div>
-      <button className="btn btn-primary" onClick={double}>
-        {/* jsx에서는 class를 js에서 이미 사용하고 있기 때문에 className */}
-        Submit
-      </button>
-    </>
+    <div className="container">
+      <div className="mb-3">
+        <label className="form-label">Title</label>
+        <input
+          className="form-control"
+          value={title}
+          onChange={(event) => {
+            setTitle(event.target.value);
+          }}
+        />
+      </div>
+      <button className="btn btn-primary">Post</button>
+    </div>
   );
 }
 export default App;
