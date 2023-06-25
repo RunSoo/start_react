206bf3ed0c73f36759708bcfb462c20aa3694515
Runsoo <imapples@naver.com>
2023-06-25 16:34:33 +0900

    11 Textarea State 바인딩

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -2,6 +2,10 @@ import { useState } from "react";
 
 function App() {
   const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const onSubmit = () => {
+    console.log(title, body);
+  };
 
   return (
     <div className="container">
@@ -15,7 +19,20 @@ function App() {
           }}
         />
       </div>
-      <button className="btn btn-primary">Post</button>
+      <div className="mb-3">
+        <label className="form-label">Body</label>
+        <textarea
+          className="form-control"
+          value={body}
+          onChange={(event) => {
+            setBody(event.target.value);
+          }}
+          rows="20"
+        />
+      </div>
+      <button className="btn btn-primary" onClick={onSubmit}>
+        Post
+      </button>
     </div>
   );
 }
