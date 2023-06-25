0ddd6d353035a796d80ad2ef2e7ad85b99a227fc
Runsoo <imapples@naver.com>
2023-06-25 15:58:28 +0900

    07 useState(1)

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,13 +1,19 @@
 // import { Fragment } from "react";
+import { useState } from "react";
 
 function App() {
-  const printHello = () => {
-    console.log("hello");
+  const [number, setNumber] = useState(1); // react hooks 함수 중 하나
+  const double = () => {
+    setNumber(number * 2);
+    console.log(number);
+    // 리렌더링이 한번만 일어날 수 있도록 한꺼번에 처리되기 때문에
+    // 이 시점에서는 아직 업데이트가 안됨
   };
 
   return (
     <>
-      <button onClick={printHello}>Submit</button>
+      <div>{number}</div>
+      <button onClick={double}>Submit</button>
     </>
   );
 }
