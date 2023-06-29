cb8ef27c7d8eb6bc36682809f8004960f97adbc4
Runsoo <imapples@naver.com>
2023-06-29 23:42:13 +0900

    36 블로그 db에서 받아와서 화면에 보여주기

--- react-basic/src/pages/ShowPage.js
+++ react-basic/src/pages/ShowPage.js
@@ -1,9 +1,33 @@
 import { useParams } from "react-router";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 const ShowPage = () => {
-  const { id } = useParams();
-  console.log(id);
-  return <div>Show Page</div>;
+  const { id } = useParams(); // routes.js랑 같은 값으로 (id) 해야 가져올 수 있음
+  const [post, setPost] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const getPost = (id) => {
+    axios.get(`http://localhost:3002/posts/${id}`).then((res) => {
+      setPost(res.data);
+      setLoading(false);
+    });
+  };
+
+  useEffect(() => {
+    getPost(id);
+  }, []);
+
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
 };
 
 export default ShowPage;
