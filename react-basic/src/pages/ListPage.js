02e12df95df9633e53598c0a6833ad18b481a0f3
Runsoo <imapples@naver.com>
2023-07-01 15:16:47 +0900

    47 admin 페이지 route 추가 및 BlogList 컴포넌트 만들기

--- react-basic/src/pages/ListPage.js
+++ react-basic/src/pages/ListPage.js
@@ -1,79 +1,12 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
-import Card from "../components/Card";
-import { Link } from "react-router-dom";
-import { useHistory } from "react-router";
-import LoadingSpinner from "../components/LoadingSpinner";
+import BlogList from "../components/BlogList";
 
 const ListPage = () => {
-  const history = useHistory();
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  const getPosts = () => {
-    axios.get("http://localhost:3002/posts").then((res) => {
-      console.log(res.data);
-      setPosts(res.data);
-      setLoading(false);
-    });
-  };
-
-  useEffect(() => {
-    getPosts(); // state가 업데이트 돼도 처음 한번만 실행
-  }, []);
-
-  const deleteBlog = (e, id) => {
-    e.stopPropagation();
-    axios.delete(`http://localhost:3002/posts/${id}`).then(() => {
-      setPosts((prevPosts) => prevPosts.filter((post) => post.id !== id)); // 같지 않을 때만 남겨두기
-    });
-  };
-
-  const renderBlogList = () => {
-    if (loading) {
-      return <LoadingSpinner />;
-    }
-
-    if (posts.length === 0) {
-      return <div>No blog post found</div>;
-    }
-    return posts
-      .filter((post) => {
-        return post.publish;
-      })
-      .map((post) => {
-        return (
-          <Card
-            key={post.id}
-            title={post.title}
-            onClick={() => history.push(`/blogs/${post.id}`)}
-          >
-            <div>
-              <button
-                className="btn btn-danger btn-sm"
-                onClick={(e) => {
-                  deleteBlog(e, post.id);
-                }}
-              >
-                Delete
-              </button>
-            </div>
-          </Card>
-        );
-      });
-  };
-
   return (
     <div>
       <div className="d-flex justify-content-between">
         <h1>Blogs</h1>
-        <div>
-          <Link to="/blogs/create" className="btn btn-success">
-            Create New
-          </Link>
-        </div>
       </div>
-      {renderBlogList()}
+      <BlogList />
     </div>
   );
 };
