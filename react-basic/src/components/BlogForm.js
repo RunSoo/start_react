9a6922342ef4e5eab14648815c773468c1dd23c0
Runsoo <imapples@naver.com>
2023-07-01 14:41:37 +0900

    45 수정 페이지 폼 5

--- react-basic/src/components/BlogForm.js
+++ react-basic/src/components/BlogForm.js
@@ -11,6 +11,8 @@ const BlogForm = ({ editing }) => {
   const [originalTitle, setOriginalTitle] = useState("");
   const [body, setBody] = useState("");
   const [originalBody, setOriginalBody] = useState("");
+  const [publish, setPublish] = useState(false);
+  const [originalPublish, setOriginalPublish] = useState(false);
 
   useEffect(() => {
     if (editing) {
@@ -19,12 +21,18 @@ const BlogForm = ({ editing }) => {
         setOriginalTitle(res.data.title);
         setBody(res.data.body);
         setOriginalBody(res.data.body);
+        setPublish(res.data.publish);
+        setOriginalPublish(res.data.publish);
       });
     }
   }, [id, editing]);
 
   const isEdited = () => {
-    return title !== originalTitle || body !== originalBody;
+    return (
+      title !== originalTitle ||
+      body !== originalBody ||
+      publish !== originalPublish
+    );
   };
 
   const goBack = () => {
@@ -35,15 +43,22 @@ const BlogForm = ({ editing }) => {
     }
   };
 
+  const onChangePublish = (e) => {
+    console.log(e.target.checked);
+    setPublish(e.target.checked);
+  };
+
   const onSubmit = () => {
     if (editing) {
       axios
         .patch(`http://localhost:3002/posts/${id}`, {
           title,
           body,
+          publish,
         })
         .then((res) => {
           console.log(res);
+          history.push(`/blogs/${id}`);
         });
     } else {
       axios
@@ -51,6 +66,7 @@ const BlogForm = ({ editing }) => {
           title,
           body,
           createdAt: Date.now(),
+          publish,
         })
         .then(() => {
           history.push("/blogs");
@@ -81,6 +97,15 @@ const BlogForm = ({ editing }) => {
           }}
           rows="20"
         />
+      </div>
+      <div className="form-check mb-3">
+        <input
+          className="form-check-input"
+          type="checkbox"
+          checked={publish}
+          onChange={onChangePublish}
+        />
+        <label className="form-check-label">Publish</label>
       </div>
       <button
         className="btn btn-primary"
