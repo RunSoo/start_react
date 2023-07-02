bcb205328ed4379859d86058ef3bafba0b705433
Runsoo <imapples@naver.com>
2023-07-03 00:10:14 +0900

    61 검색 기능 1

--- react-basic/src/components/BlogList.js
+++ react-basic/src/components/BlogList.js
@@ -3,7 +3,7 @@ import { useState, useEffect, useCallback } from "react";
 import Card from "../components/Card";
 import { useHistory } from "react-router";
 import LoadingSpinner from "../components/LoadingSpinner";
-import { bool } from "prop-types";
+import propTypes from "prop-types";
 import Pagination from "./Pagination";
 import { useLocation } from "react-router-dom";
 
@@ -17,7 +17,8 @@ const BlogList = ({ isAdmin }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const [numberOfPosts, setNumberOfPosts] = useState(0);
   const [numberOfPages, setNumberOfPages] = useState(0);
-  const limit = 1;
+  const [searchText, setSearchText] = useState("");
+  const limit = 5;
 
   useEffect(() => {
     setNumberOfPages(Math.ceil(numberOfPosts / limit));
@@ -34,6 +35,7 @@ const BlogList = ({ isAdmin }) => {
         _limit: limit,
         _sort: "id",
         _order: "desc",
+        title_like: searchText,
       };
 
       if (!isAdmin) {
@@ -49,7 +51,7 @@ const BlogList = ({ isAdmin }) => {
           setLoading(false);
         });
     },
-    [isAdmin]
+    [isAdmin, searchText]
   );
 
   useEffect(() => {
@@ -72,9 +74,6 @@ const BlogList = ({ isAdmin }) => {
     return <LoadingSpinner />;
   }
 
-  if (posts.length === 0) {
-    return <div>No blog post found</div>;
-  }
   const renderBlogList = () => {
     return posts.map((post) => {
       return (
@@ -100,22 +99,41 @@ const BlogList = ({ isAdmin }) => {
     });
   };
 
+  const onSearch = () => {
+    getPosts(1);
+  };
+
   return (
     <div>
-      {renderBlogList()}
-      {numberOfPages > 1 && (
-        <Pagination
-          currentPage={currentPage}
-          numberOfPages={numberOfPages}
-          onClick={onClickPageButton}
-        />
+      <input
+        type="text"
+        className="form-control"
+        placeholder="search"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+        onKeyUp={onSearch}
+      />
+      <hr />
+      {posts.length === 0 ? (
+        <div>No blog post found</div>
+      ) : (
+        <>
+          {renderBlogList()}
+          {numberOfPages > 1 && (
+            <Pagination
+              currentPage={currentPage}
+              numberOfPages={numberOfPages}
+              onClick={onClickPageButton}
+            />
+          )}
+        </>
       )}
     </div>
   );
 };
 
 BlogList.propTypes = {
-  isAdmin: bool,
+  isAdmin: propTypes.bool,
 };
 
 BlogList.defaultProps = {
